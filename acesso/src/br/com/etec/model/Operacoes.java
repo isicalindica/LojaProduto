166a777f89bcd87f7b2a8847c58b5586b4918921
lab04 <lab04@lab4-01>
2024-08-19 22:47:05 -0300

    CRIADO TELA DE LOGIN COM CODIGO

--- acesso/src/br/com/etec/model/Operacoes.java
+++ acesso/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,47 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
 	
+	@FXML
+	private TextField txfUsuario;
+	@FXML
+	private PasswordField psfUsuario;
+	@FXML
+	private Button btnAcessar;
+	@FXML
+	private void acessarConta(ActionEvent event) {
+		
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		
+		String senhaUsuario;
+		senhaUsuario = psfUsuario.getText();
+		
+		if(nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+			mostrarMensagem(Alert.AlertType.WARNING, "FALTANDO DADOS", "SEM USUÁRIO OU SENHA");
+		}// if
+		else {
+			mostrarMensagem(Alert.AlertType.WARNING,"FALTANDO DADOS", "INFORMAR A SENHA");
+		}
+		
+		
+	};// acessarConta
+	
+	//-----------------------------
+	
+	
+	private void mostrarMensagem(Alert.AlertType tipo, String titulo, String mensagem) {
+		Alert alerta = new Alert(tipo);
+		alerta.setTitle(titulo);
+		alerta.setHeaderText(null);
+		alerta.setContentText(mensagem);
+		alerta.showAndWait();
+	}
 }
